dce0a77a8290f27f4ff87408252e6452ebca2b32
Manreet Kaur <78394716+manreett@users.noreply.github.com>
2021-11-21 23:17:04 -0500

    Add files via upload

---
+++ Exercise_1/CompanyProject/CommonUtility/src/main/java/CommonUtility.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+package com.mycompany;
+public class CommonUtility {
+    public static String getAppName() {
+        return "My Company App standard version";
+    }
+    
+}
